5e500947065fa0e4a3116c5d453ae8aa5a878162
Cristian Vargas <cristian.v0223@gmail.com>
2022-09-18 16:22:44 -0500

    removed excess files

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -16,6 +16,7 @@ import Auth from '../utils/auth';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(QUERY_ME);
+  // eslint-disable-next-line
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
@@ -30,6 +31,7 @@ const SavedBooks = () => {
     }
 
     try {
+      // eslint-disable-next-line
       const { data } = await removeBook({
         variables: { bookId },
       });
